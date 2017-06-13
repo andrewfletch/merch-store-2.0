969da49b24f06a462085ba641bbdb1fb33ee360a
Andrew Fletcher <andrewsfletch@gmail.com>
2017-06-13 18:46:43 +0000

    Changed css, and overrides for billing form

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,6 +13,8 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+  config.logo = 'logo_crop.png'
+  config.admin_interface_logo = 'logo_long_png.png'
 end
 
 Spree.user_class = "Spree::User"
